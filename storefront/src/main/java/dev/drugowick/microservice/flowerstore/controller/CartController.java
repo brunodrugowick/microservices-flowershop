1e7551372d280341f8659cd629e43f784385fa46
brunodrugowick <bruno.drugowick@gmail.com>
2019-11-15 16:19:34 -0300

    Removes unused import.

--- storefront/src/main/java/dev/drugowick/microservice/flowerstore/controller/CartController.java
+++ storefront/src/main/java/dev/drugowick/microservice/flowerstore/controller/CartController.java
@@ -2,7 +2,6 @@ package dev.drugowick.microservice.flowerstore.controller;
 
 import dev.drugowick.microservice.flowerstore.dto.CartDTO;
 import dev.drugowick.microservice.flowerstore.service.CartService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
