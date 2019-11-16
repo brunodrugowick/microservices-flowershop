f8b208332e19b086d97884f94918982cd5232dcc
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-11-16 16:06:36 -0300

    Shows how client-side load-balancing works.
    This commit only adds a DiscoveryClient (notice the correct interface).
    This shows, on every request, all of its cached clients.

--- storefront/src/main/java/dev/drugowick/microservice/flowerstore/service/CartService.java
+++ storefront/src/main/java/dev/drugowick/microservice/flowerstore/service/CartService.java
@@ -2,7 +2,10 @@ package dev.drugowick.microservice.flowerstore.service;
 
 import dev.drugowick.microservice.flowerstore.dto.CartDTO;
 import dev.drugowick.microservice.flowerstore.dto.SupplierInfoDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,12 +18,17 @@ public class CartService {
     private String supplierUrl;
 
     private RestTemplate restTemplate;
+    private DiscoveryClient discoveryClient;
+    
+    public CartService(RestTemplate restTemplate, DiscoveryClient discoveryClient) {
+		super();
+		this.restTemplate = restTemplate;
+		this.discoveryClient = discoveryClient;
+	}
 
-    public CartService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
-    public void finishCart(CartDTO cartDTO) {
+
+	public void finishCart(CartDTO cartDTO) {
 
         ResponseEntity<SupplierInfoDTO> response = restTemplate.exchange(
                 supplierUrl + "/info/" + cartDTO.getAddress().getProvince(),
@@ -28,5 +36,14 @@ public class CartService {
                 null,
                 SupplierInfoDTO.class
         );
+        
+        /**
+         * Just out of curiosity it's possible to see the available clients that Ribbon is load-balancing to.
+         */
+        discoveryClient.getInstances("supplier").stream()
+        .forEach(supplier -> {
+        	System.out.println(supplier.getHost() + ":" + supplier.getPort());
+        });
+        
     }
 }
